cf5643e7f78fc0bcb5646472e1ab630f9b8d0ff6
bhoomikagoel24 <bhoomikagoel24@gmail.com>
2026-03-23 09:18:32 +0530

    custom prompt setup'

--- TriMind_RAG_Engine/chains/conversational_chain.py
+++ TriMind_RAG_Engine/chains/conversational_chain.py
@@ -1,10 +1,13 @@
 from langchain_classic.chains import ConversationalRetrievalChain
 from TriMind_RAG_Engine.memory.conversation_memory import get_conversation_memory
-
+from TriMind_RAG_Engine.prompts.rag_prompt_template import CONVERSATIONAL_PROMPT
 
 def create_conversational_chain(llm, retriever):
     """
-    Creates a Conversational RAG chain with short-term memory.
+    Creates a Conversational RAG chain with:
+    - short-term memory
+    - custom prompt
+    - better response
     """
 
     # Create memory
@@ -16,7 +19,12 @@ def create_conversational_chain(llm, retriever):
         retriever=retriever,
         memory=memory,
         return_source_documents=True,
-        verbose=False
+        verbose=False,
+        combine_docs_chain_kwargs={
+            "prompt":CONVERSATIONAL_PROMPT,   # giving controlled response
+            # "document_varible_name": "context"
+            # Custom Domain Aware prompt is being used 
+        }
     )
 
     return chain
